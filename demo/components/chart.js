565d201248f20001516cfb08d318f6de11c06129
freeman-lab <the.freeman.lab@gmail.com>
2020-08-08 16:44:21 -0700

    expand demo

---
+++ demo/components/chart.js
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react'
+import { Vega } from 'react-vega'
+import { useThemeUI, Box } from 'theme-ui'
+
+var vegaLite = require('vega-lite')
+
+const Chart = ({ x, y, scales }) => {
+  const [spec, setSpec] = useState(null)
+  const [loaded, setLoaded] = useState(false)
+  const context = useThemeUI()
+  const theme = context.theme
+
+  const data = x.map((v, i) => {
+    return {
+      x: v,
+      y: y[i]
+    }
+  })
+
+  useEffect(() => {
+    const config = {
+      background: null,
+      padding: { left: 0, right: 0, top: 0, bottom: 0 },
+      axis: {
+        grid: false,
+        labelFontSize: theme.fontSizes[1],
+        labelFont: theme.fonts.monospace,
+        labelColor: theme.colors.text,
+        titleFont: theme.fonts.monospace,
+        titleFontSize: theme.fontSizes[1],
+        titleColor: theme.colors.text,
+        domain: true,
+        tickOffset: 0,
+        labelPadding: 5,
+        titlePadding: 10,
+      },
+      view: {
+        stroke: 'none',
+      },
+    }
+
+    const spec = {
+      data: {
+        name: 'values',
+      },
+      mark: {
+        type: 'line',
+        clip: true,
+      },
+      encoding: {
+        x: {
+          field: 'x',
+          type: 'quantitative',
+          scale: {
+            domain: scales.x,
+            nice: false,
+            padding: 0,
+          },
+          axis: {
+            title: null,
+            offset: 6,
+          },
+        },
+        y: {
+          field: 'y',
+          type: 'quantitative',
+          scale: {
+            domain: scales.y,
+            padding: 0,
+            nice: false,
+          },
+          axis: {
+            title: null,
+            orient: 'left',
+            padding: 0,
+            offset: 5,
+          },
+        },
+      },
+    }
+    setSpec(vegaLite.compile(spec, { config: config }).spec)
+    setLoaded(true)
+  }, [context])
+
+  const width = 280
+  const height = 150
+
+  return (
+    <>
+      {loaded && (
+        <Vega
+          width={width}
+          height={height}
+          data={{ values: data }}
+          renderer={'svg'}
+          actions={false}
+          spec={spec}
+        />
+      )}
+      {!loaded && <Box sx={{ height: height + 41 }}></Box>}
+    </>
+  )
+}
+
+export default Chart
--- demo/pages/index.js
+++ demo/pages/index.js
@@ -1,16 +1,21 @@
-import { Box, Text } from 'theme-ui'
+import { useState } from 'react'
+import { Box, Text, Slider } from 'theme-ui'
 import { Model } from '../..'
+import Chart from '../components/chart'
 
 function Index() {
-  var opts = {
+  const [B, setB] = useState(1.13)
+  const [q0, setq0] = useState(5)
+
+  const opts = {
     time: {
       tmin: 2020,
       tmax: 2200,
-      dt: 5
+      dt: 1
     },
     baseline: {
       form: 'ramp',
-      q0: 7.5,
+      q0: q0,
       q0mult: 3,
       t1: 2100,
       t2: 2150
@@ -21,7 +26,7 @@ function Index() {
       a: (6.9/2)/Math.log(2),
       Finf: 8.5,
       F0: 3,
-      B: 1.13,
+      B: B,
       Cd: 106,
       x: 0.73,
       T0: 1.1,
@@ -33,12 +38,21 @@ function Index() {
 
   const m = Model(opts)
 
-  return <Box sx={{ 
-    ml: [5], 
-    mt: [5], 
-    fontSize: [6], 
-    fontFamily: 'monospace' 
-  }}>MARGO-JS demo</Box>
+  return <Box sx={{ ml: [6], mt: [6], fontFamily: 'monospace'}}>
+    <Text sx={{ fontSize: [6] }}>MARGO-JS demo</Text>
+    <Box sx={{ my: [3] }}>
+      <Text sx={{ fontSize: [3] }}>B</Text>
+      <Slider sx={{width: '200px'}} value={B} onChange={(e) => setB(parseFloat(e.target.value))} min="1.0" max="2.0" step="0.01"></Slider>
+    </Box>
+    <Box sx={{ my: [3] }}>
+      <Text sx={{ fontSize: [3] }}>q0</Text>
+      <Slider sx={{width: '200px'}} value={q0} onChange={(e) => setq0(parseFloat(e.target.value))} min="2" max="7" step="0.1"></Slider>
+    </Box>
+    <Chart x={m.t()} y={m.emissions({units: 'CO2e'})} scales={{x: [2020, 2200], y: [-1, 190]}}/>
+    <Chart x={m.t()} y={m.concentration()} scales={{x: [2020, 2200], y: [500, 1400]}}/>
+    <Chart x={m.t()} y={m.forcing()} scales={{x: [2020, 2200], y: [0, 10]}}/>
+    <Chart x={m.t()} y={m.temperature()} scales={{x: [2020, 2200], y: [1, 5]}}/>
+  </Box>
 }
 
 export default Index
