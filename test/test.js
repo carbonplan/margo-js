c1e0bab031a3c8c430c8a5dc2efdde8c1532a4e0
freeman-lab <the.freeman.lab@gmail.com>
2020-08-16 13:05:38 -0700

    slightly reduce precision on equality check

--- test/test.js
+++ test/test.js
@@ -12,51 +12,61 @@ const modelControlled = margo.Model({
   },
 })
 
+test.Test.prototype.almostEqual = function(a, b, msg, extra) {
+  this._assert(a.toFixed(12) == b.toFixed(12), {
+    message: msg || 'should be equal up to precision of 12',
+    operator: 'equal',
+    actual: a,
+    expected: b,
+    extra: extra,
+  })
+}
+
 test('emissions (default)', (t) => {
   const emissions = modelDefault.emissions()
-  t.equal(emissions[0], 7.5)
-  t.equal(emissions[5], 12.1875)
+  t.almostEqual(emissions[0], 7.5)
+  t.almostEqual(emissions[5], 12.1875)
   t.end()
 })
 
 test('concentration (default)', (t) => {
   const concentration = modelDefault.concentration()
-  t.equal(concentration[0], 478.75)
-  t.equal(concentration[5], 607.65625)
+  t.almostEqual(concentration[0], 478.75)
+  t.almostEqual(concentration[5], 607.65625)
   t.end()
 })
 
 test('forcing (default)', (t) => {
   const forcing = modelDefault.forcing()
-  t.equal(forcing[0], 0.1988532592444071)
-  t.equal(forcing[5], 1.3855943936513244)
+  t.almostEqual(forcing[0], 0.1988532592444071)
+  t.almostEqual(forcing[5], 1.3855943936513244)
   t.end()
 })
 
 test('temperature (default)', (t) => {
   const temperature = modelDefault.temperature()
-  t.equal(temperature[0], 1.2083551842122768)
-  t.equal(temperature[5], 1.8776281942807453)
+  t.almostEqual(temperature[0], 1.2083551842122768)
+  t.almostEqual(temperature[5], 1.8776281942807453)
   t.end()
 })
 
 test('ecs (default)', (t) => {
-  t.equal(modelDefault.ecs(), 3.053097345132744)
+  t.almostEqual(modelDefault.ecs(), 3.053097345132744)
   t.end()
 })
 
 test('growth (default)', (t) => {
   const growth = modelDefault.growth()
-  t.equal(growth[0], 100)
-  t.equal(growth[5], 164.06059944647308)
+  t.almostEqual(growth[0], 100)
+  t.almostEqual(growth[5], 164.06059944647308)
   t.end()
 })
 
 test('discount (default)', (t) => {
   const discount = modelDefault.discount()
-  t.equal(discount[0], 0)
-  t.equal(discount[1], 0.9514656876067488)
-  t.equal(discount[5], 0.7797684429937835)
+  t.almostEqual(discount[0], 0)
+  t.almostEqual(discount[1], 0.9514656876067488)
+  t.almostEqual(discount[5], 0.7797684429937835)
   t.end()
 })
 
@@ -124,9 +134,9 @@ test('opts (default)', (t) => {
 test('update', (t) => {
   const m = margo.Model()
 
-  t.equal(m.ecs(), 3.053097345132744)
+  t.almostEqual(m.ecs(), 3.053097345132744)
   m.physics = { B: 1.2 }
-  t.equal(m.ecs(), 2.8750000000000004)
+  t.almostEqual(m.ecs(), 2.8750000000000004)
 
   t.equal(m.t()[0], 2020)
   m.time = { tmin: 2030 }
